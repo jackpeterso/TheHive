758e231f61b3c13491dd8543ac7a20d75f785206
Jack Peterson <peteja@student.breckschool.org>
2018-05-03 13:12:14 -0500

    si
    si

--- TheHive/manage.py
+++ TheHive/manage.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python
-import os
-import sys
+from django.db import models
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thehive.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
